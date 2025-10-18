65625abad760c0f1cf74dafe09f12cfcb6c77959
Etienne Schalk <etienne.schalk@gmail.com>
2025-10-18 18:53:43 +0200

    Set up pre-commit and github actions

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    // Base ESLint rules
+    'semi': 'warn',
+    'curly': 'warn',
+    'eqeqeq': 'warn',
+    'no-throw-literal': 'warn',
+    
+    // TypeScript-specific rules
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-var-requires': 'error',
+  },
+  ignorePatterns: [
+    'out',
+    'dist',
+    '**/*.d.ts',
+    'node_modules',
+    '.vscode-test'
+  ]
+};
